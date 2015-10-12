9de3d06272a0da507ef8ad0e3ff720e1d107c4a7
Ryan M Bucinell <rbucinell.dev@gmail.com>
2015-10-12 18:35:22 -0400

    Code cleanup
    Ran JS Lint, adjusted tab length, cleaned trailing whitespace, added
    method header stubs

--- Box.js
+++ Box.js
@@ -3,16 +3,18 @@ var AREA_POWER = 0.25;
 var MAX_DIVISIONS = 10;
 var MAX_LINE_WIDTH = 4;
 
-DrawMode ={ 
-	BOX: "Box", 
-	BOXBORDER: "BoxBorder", 
-	HATCH: "Hatch",
-	HATCHSHADE: "HatchShaded"
+DrawMode = {
+	BOX:		"Box",
+	BOXBORDER: 	"BoxBorder",
+	HATCH:		"Hatch",
+	HATCHSHADE:	"HatchShaded"
 };
 
-
+/**
+*	Box constructor
+**/
 function Box( image, parent, x, y, w, h, depth )
-{	
+{
 	this.parent = parent;
 	this.image = image;
 	this.x = x;
@@ -20,12 +22,13 @@ function Box( image, parent, x, y, w, h, depth )
 	this.w = w;
 	this.h = h;
 	this.depth = depth;
+	this.hatchWidth =  Math.tan( Math.atan( MAX_LINE_WIDTH / MAX_DEPTH ) )* ( MAX_DEPTH - this.depth);
 	this.children = [];
-	this.hist = new Histogram( image, x, y, w, h );		
+	this.hist = new Histogram( image, x, y, w, h );
 	this.setColor( this.hist.averageColor() );
 	this.score = -this.error * Math.pow( this.area(), AREA_POWER );
 	this.mode = DrawMode.HATCHSHADE;
-};
+}
 
 /**
 * Determines if there are no children to this node
@@ -33,7 +36,9 @@ function Box( image, parent, x, y, w, h, depth )
 Box.prototype.isLeaf = function()
 {
 	if( this.children.length === 0 )
+	{
 		return true;
+	}
 	return false;
 };
 
@@ -43,17 +48,22 @@ Box.prototype.isLeaf = function()
 Box.prototype.getLeafNodes = function()
 {
 	if( this.children === [] )
+	{
 		return [ this ];
+	}
 	else
+	{
 		return this.children;
+	}
 };
 
 Box.prototype.getAllLeafNodes = function( currentlist, maxdepth )
 {
 	if( this.depth >= maxdepth )
 	{
-		
-	}else if( this.isLeaf() )
+
+	}
+	else if( this.isLeaf() )
 	{
 		currentlist.push( this );
 	}
@@ -62,10 +72,10 @@ Box.prototype.getAllLeafNodes = function( currentlist, maxdepth )
 		for( var i = 0; i < this.children.length; i++ )
 		{
 			currentlist = this.children[i].getAllLeafNodes( currentlist, maxdepth );
-		}		
+		}
 	}
 	return currentlist;
-}
+};
 
 /**
 * Gets the children nodes to this box up to a maximum depth given
@@ -74,7 +84,6 @@ Box.prototype.getLeafNodes = function( depth )
 {
 	if( this.depth >= depth )
 	{
-		//this.error = 0;
 		return [ this ];
 	}
 	else if( this.children === [])
@@ -85,14 +94,14 @@ Box.prototype.getLeafNodes = function( depth )
 	{
 		return this.children;
 	}
-}
+};
 
 /**
 * Generate children nodes for this box
 **/
 Box.prototype.divide = function()
-{	
-	
+{
+
 	var w2 = this.w / 2;
 	var h2 = this.h / 2;
 
@@ -103,130 +112,66 @@ Box.prototype.divide = function()
 
 	this.children =[ q1, q2, q3, q4 ];
 };
-	
+
 /**
 * Draw the box's current color and if enabled, a border
 **/
 Box.prototype.draw = function( ctx )
 {
-	ctx.fillStyle = this.color;	
-	
+	ctx.fillStyle = this.color;
+
 	switch( this.mode )
-	{		
+	{
 		case DrawMode.BOXBORDER:
 			ctx.beginPath();
 			ctx.lineWidth="0.1";
 			ctx.strokeStyle= "black";//this.color;
 			ctx.rect( this.x, this.y, this.w, this.h );
 			ctx.stroke();
+		/* falls through */
 		case DrawMode.BOX:
-			ctx.fillRect( this.x, this.y, this.w, this.h );			
+			ctx.fillRect( this.x, this.y, this.w, this.h );
 			break;
 		case DrawMode.HATCHSHADE:
 			ctx.globalAlpha = 0.2;
 			ctx.fillRect( this.x, this.y, this.w, this.h );
 			ctx.globalAlpha = 1;
+		/* falls through */
 		case DrawMode.HATCH:
 			//determine the number of hatches in a square
-			var divisions = MAX_DIVISIONS / (this.depth + 1);		
+			var divisions = MAX_DIVISIONS / (this.depth + 1);
 			var vHatches = this.w / divisions;
 			var hHatches = this.h / divisions;
-			
-			//The deeper we go, the finer our hatches become		
-			ctx.lineWidth = Math.tan( Math.atan( MAX_LINE_WIDTH / MAX_DEPTH ) )* ( MAX_DEPTH - this.depth);	
+
+			//The deeper we go, the finer our hatches become
+			ctx.lineWidth = this.hatchWidth;
 			ctx.strokeStyle = this.color;
-			
-			ctx.beginPath();	
-			for( var i = 0; i < divisions; i++ )
-			{			
-				
+
+			ctx.beginPath();
+
+
+			var i = 0;
+			for( i = 0; i < divisions; i++ )
+			{
 				//Verticle hatches
 				ctx.moveTo(this.x+ i*vHatches, this.y);
 				ctx.lineTo(this.x+ i*vHatches, this.y+ this.h);
+
 				//Horizontal hatches
 				ctx.moveTo(this.x, this.y+ i*hHatches);
 				ctx.lineTo(this.x + this.w, this.y+ i*hHatches);
-				
-				
+
 			}
 			ctx.stroke();
-			
-			for( var i = 0; i < divisions; i++ )
+
+			for( i = 0; i < divisions; i++ )
 			{
 				ctx.beginPath();
-				
 				ctx.stroke();
 			}
 			break;
 	}
-	
-	/*
-	if( drawHatches )
-	{
-		if( hatchShading )
-		{
-			ctx.fillStyle = this.color;
-			ctx.globalAlpha = 0.2;
-			ctx.fillRect( this.x, this.y, this.w, this.h );
-			ctx.globalAlpha = 1;
-		}			
-		
-		var divisions = 10 / this.depth;
-		
-		var vHatches = this.w / divisions;
-		var hHatches = this.h / divisions;
-		
-		ctx.lineWidth="1";
-		
-		var thickestLineWidth = 4;
-		var widthOfLine = Math.tan( Math.atan( thickestLineWidth / MAX_DEPTH ) )* ( MAX_DEPTH - this.depth);
-		ctx.lineWidth=widthOfLine;		
-		
-		ctx.strokeStyle = this.color;
-		
-		for( var i = 0; i < divisions; i++ )
-		{			
-			ctx.beginPath();	
-			//Verticle hatches
-			ctx.moveTo(this.x+ i*vHatches, this.y);
-			ctx.lineTo(this.x+ i*vHatches, this.y+ this.h);
-			//Horizontal hatches
-			ctx.moveTo(this.x, this.y+ i*hHatches);
-			ctx.lineTo(this.x + this.w, this.y+ i*hHatches);
-			
-			ctx.stroke();
-		}
-		
-		
-		for( var i = 0; i < divisions; i++ )
-		{
-			ctx.beginPath();
-			
-			ctx.stroke();
-		}
-	}
-	else
-	{
-		ctx.fillStyle = this.color;
-		ctx.fillRect( this.x, this.y, this.w, this.h );
-	}
-	
-	if( drawLines )
-	{
-		ctx.beginPath();
-		ctx.lineWidth="0.1";
-		ctx.strokeStyle= "black";//this.color;
-		ctx.rect( this.x, this.y, this.w, this.h );
-		ctx.stroke();
-	}
-	
-	//Debug code, remove
-	if( DEBUG_MODE )
-	{
-		ctx.stroke();
-		ctx.fillStyle = "#000";
-		ctx.fillText("e:" + Math.floor(this.error), this.x+5, this.y+15 );
-	}*/
+
 };
 
 /**
--- quandrants.js
+++ quandrants.js
@@ -3,7 +3,7 @@ ITERATIONS = 1024;
 MAX_DEPTH = 8;
 
 //Class public vars
-var ERROR_RATE = .5;
+var ERROR_RATE = 0.5;
 var DEBUG_MODE = false;
 
 //The Image data
@@ -13,7 +13,7 @@ var g_imageData; //will be set on g_image.OnLoad
 //Canvas used for drawing the actual image
 var canvas;
 var ctx;
-			
+
 //Buffer
 var bufferCanvas = document.createElement('canvas');
 var buffer = bufferCanvas.getContext('2d');
@@ -25,7 +25,7 @@ function quandrants( passed_canvas )
 	ctx = canvas.getContext("2d");
 	bufferCanvas.width 	= canvas.width;
 	bufferCanvas.height = canvas.height;
-	
+
 	// Member variables
 	var intervalID	= -1;
 	var framerate 	= 20; //60
@@ -35,8 +35,8 @@ function quandrants( passed_canvas )
 	var root;
 	var drawNode;
 	var hist;
-	
-	
+
+
 	var previousError = -1;
 	var error = 0;
 	var errorSum;
@@ -44,38 +44,47 @@ function quandrants( passed_canvas )
 	* Initializes the image on the screen
 	**/
 	this.initialize = function( img )
-	{	
+	{
 		g_image = new Image();// = img;
 		g_image.onload = function()
-		{			
+		{
 			//Draw original image to get image data
 			ctx.drawImage( g_image, 0, 0, g_image.width, g_image.height);//, 0, 0, canvas.width, canvas.height);
 			g_imageData = ctx.getImageData(0,0,canvas.width, canvas.height);
 			ctx.clearRect( 0, 0, canvas.width, canvas.height);
-			
+
 			//Create the starting root node
 			root = new Box( g_image, {},  0, 0, canvas.width, canvas.height, 0 );
 			errorSum = root.error * root.area();
-			
+
 			//Initial Draw
 			drawNode = root;
 			boxes.push( root );
-			draw();	
-		}
+			draw();
+		};
 		g_image.src = img.src;
-	}
-	
+	};
+
+	/**
+	*
+	**/
 	this.Running = function()
 	{
 		return isRunning;
-	}
-	
+	};
+
+	/**
+	*
+	**/
 	this.play = function()
 	{
 		isRunning = true;
 		intervalID = setInterval( running, 1000/ framerate );
-	}
-	
+	};
+
+	/**
+	*
+	**/
 	this.step = function()
 	{
 		isRunning = true;
@@ -84,8 +93,8 @@ function quandrants( passed_canvas )
 			ITERATIONS = 1;
 		}
 		running();
-	}
-	
+	};
+
 	/**
 	* Stops the execution of code
 	*/
@@ -93,8 +102,8 @@ function quandrants( passed_canvas )
 	{
 		clearInterval(intervalID);
 		isRunning = false;
-	}
-	
+	};
+
 	/**
 	*	Basic game loop will keep iterating until flagged to stop
 	**/
@@ -103,10 +112,10 @@ function quandrants( passed_canvas )
 		if( isRunning && ITERATIONS > 0 )
 		{
 			update();
-			draw();	
-		}	
+			draw();
+		}
 	}
-	
+
 	/**
 	*	A compareTo function to sort Box's by their error
 	**/
@@ -122,8 +131,8 @@ function quandrants( passed_canvas )
 			return 1;
 		}
 		return 0;
-	};
-	
+	}
+
 	/**
 	*	Game loop update function
 	**/
@@ -131,12 +140,12 @@ function quandrants( passed_canvas )
 	{
 		var index =  -1;
 		var largestError = 0;
-		
+
 		error = averageErrorOfQuad( drawNode, errorSum );
-		
+
 		if( previousError == -1 || previousError - error > ERROR_RATE )
 			previousError = error;
-		
+
 		var leaves = root.getAllLeafNodes( [], MAX_DEPTH );
 		leaves.sort( errorCompareTo );
 		var last = leaves[0];// leaves[leaves.length-1];
@@ -150,23 +159,29 @@ function quandrants( passed_canvas )
 				errorSum += kids[child].error + kids[child].area();
 			}
 		}
-		
-		
+
+
 		drawNode = last;
 		boxes = leaves;
-		
-		ITERATIONS -= 1;		
-	};
-	
+
+		ITERATIONS -= 1;
+	}
+
+	/**
+	*
+	**/
 	function averageErrorOfQuad( quad, errorsum )
 	{
 		return errorsum / quad.area();
 	}
-	
+
+	/**
+	*
+	**/
 	function getBoxWithLargestError( box )
 	{
 		if( box.isLeaf() || box.depth >= MAX_DEPTH )
-		{			
+		{
 			return box;
 		}
 		else
@@ -174,17 +189,15 @@ function quandrants( passed_canvas )
 			var children = box.getLeafNodes();
 			children.sort( Box.errorCompareT );
 			var largestChild = children[children.length-1];
-			//var largestChild = Math.max.apply( Math, box.getLeafNodes().map(function(b){ return b.error;}));			
 			return getBoxWithLargestError( largestChild );
 		}
 	}
-	
-	
+
 	/**
 	*	Game loop draw function
 	**/
 	function draw()
-	{		
+	{
 		drawNode.draw(ctx);
 		for( var i = 0; i < drawNode.children.length; i++ )
 		{
@@ -192,7 +205,7 @@ function quandrants( passed_canvas )
 		}
 		return;
 	}
-	
+
 	/**
 	*	Gets the average color of a area
 	**/
@@ -201,24 +214,24 @@ function quandrants( passed_canvas )
 		var imgData = buffer.getImageData( x, y, w, h );
 		var total = imgData.data.length;
 		var R = 0, G = 0, B = 0;
-		
+
 		//Manipulate the current section
 		for( var i = 0; i < total; i+= 4 )
 		{
 			//Index of RGBA
-			var r = i + 0, 
-				g = i + 1, 
-				b = i + 2, 
+			var r = i + 0,
+				g = i + 1,
+				b = i + 2,
 				a = i + 3;
-			
+
 			R = R + imgData.data[r];
 			G = G + imgData.data[g];
-			B = B + imgData.data[b];			
+			B = B + imgData.data[b];
 		}
 		return {
 			r:Math.floor(R/total),
 			g:Math.floor(G/total),
 			b:Math.floor(B/total)
-		};		
-	}	
+		};
+	}
 }
